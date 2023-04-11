4cd0e9f8a162e23ff877fba1ded574a5578c0e8b
leeks123 <kensei719@naver.com>
2023-04-12 00:30:17 +0900

    src1

---
+++ src1/Payment.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { usePaymentMethods } from "./usePaymentMethods.hook";
+
+export const Payment = ({ amount }: { amount: number }) => {
+  const { paymentMethods } = usePaymentMethods();
+
+  return (
+    <div>
+      <h3>Payment</h3>
+      <div>
+        {paymentMethods.map((method) => (
+          <label key={method.provider}>
+            <input
+              type="radio"
+              name="payment"
+              value={method.provider}
+              defaultChecked={method.provider === "cash"}
+            />
+            <span>{method.label}</span>
+          </label>
+        ))}
+      </div>
+      <button>${amount}</button>
+    </div>
+  );
+};
